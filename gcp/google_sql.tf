c2e9cfda19067f000e7a13490e46c507a817a932
satwik <satwik@oak9.io>
2023-01-31 12:00:33 -0600

    More updates

--- gcp/google_sql.tf
+++ gcp/google_sql.tf
@@ -36,3 +36,42 @@ resource "google_sql_database_instance" "postgres" {
     }
   }
 }
+
+resource "google_sql_database_instance" "mysql" {
+  name             = "mysql-instance-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_5_6"
+
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+
+      ipv4_enabled = true
+
+      dynamic "authorized_networks" {
+        for_each = google_compute_instance.apps
+        iterator = apps
+
+        content {
+          name  = apps.value.name
+          value = apps.value.network_interface.0.access_config.0.nat_ip
+        }
+      }
+
+      dynamic "authorized_networks" {
+        for_each = local.onprem
+        iterator = onprem
+
+        content {
+          name  = "onprem-${onprem.key}"
+          value = onprem.value
+        }
+      }
+      require_ssl = false
+    }
+
+    backup_configuration {
+      point_in_time_recovery_enabled = false
+    }
+  }
+}
